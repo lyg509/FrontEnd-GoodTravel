4f4be4aee1cf8aa54e864a687e00a1e8f6953fab
lyg509 <dydrjs252@naver.com>
2023-02-13 18:30:08 +0900

    코스등록 기능구현

--- store/index.ts
+++ store/index.ts
@@ -2,16 +2,28 @@ import { combineReducers } from 'redux';
 import { all } from 'redux-saga/effects';
 import course, { courseSaga, CourseState } from './course';
 import travel, { travelSaga, TravelState } from './travel';
+import location, { locationSaga, LocationState } from './location';
 import user, { userSaga, UserState } from './user';
+import register, { registerSaga, RegisterState } from './register';
 
 export function* rootSaga() {
-  yield all([userSaga(), courseSaga(), travelSaga()]);
+  yield all([
+    userSaga(),
+    courseSaga(),
+    travelSaga(),
+  
+    locationSaga(),
+    registerSaga(),
+  ]);
 }
 
 const rootReducer = combineReducers({
   user,
   course,
   travel,
+
+  location,
+  register,
 });
 
 export default rootReducer;
@@ -20,4 +32,6 @@ export type RootState = {
   user: UserState;
   course: CourseState;
   travel: TravelState;
+  location: LocationState;
+  register: RegisterState;
 };
