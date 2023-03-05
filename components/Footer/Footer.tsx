8b7a4eefc7c63cfdf8e99e3fddc94d1e28c25a17
lyg509 <dydrjs252@naver.com>
2023-03-05 20:02:14 +0900

    Footter 수정

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,30 +1,11 @@
 import type { NextPage } from 'next';
-import { Wrapper, ChatIcons } from './Footer.style';
-import { AiFillMessage } from 'react-icons/ai';
-import { RootState } from '../../store';
-import { useDispatch, useSelector } from 'react-redux';
-import { setModalState } from '../../store/chat';
-import ChatApp from '../Chats/ChatApp';
+import { Wrapper } from './Footer.style';
 const Footer: NextPage = () => {
-  const dispatch = useDispatch();
-  const { tourId } = useSelector((state: RootState) => state.record); // 여행중인지 체크(채팅)
-  const showModal = () => {
-    dispatch(setModalState(true));
-  };
   return (
     <>
-      {tourId != 0 && (
-        <>
-          <ChatIcons>
-            <AiFillMessage onClick={showModal} />
-          </ChatIcons>
-          <ChatApp />
-        </>
-      )}
-
       <Wrapper>
         <div>Copyright ⓒ 여행어때 Corp. All rights reserved</div>
-        <div>주소 : 성남시 수정구 태평동</div>
+        <div>주소 : 경기도 성남시 수정구 태평동</div>
         <div>email : 여행어때@naver.com</div>
       </Wrapper>
     </>
